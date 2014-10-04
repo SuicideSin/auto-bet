2622e00d514d3de0fb19336e1d7bac3cfcf05444
Timothy Zorn <tim@zornco.com>
2014-10-03 22:00:04 -0400

    we don't need an updateURL - downloadURL is sufficient

--- auto-bet.user.js
+++ auto-bet.user.js
@@ -4,14 +4,13 @@
 // @description	  This will use the most recent version of the auto-bet script at all times
 // @author        SystemDisc
 // @downloadURL   https://raw.githubusercontent.com/SystemDisc/auto-bet/master/auto-bet.user.js
-// @updateURL     https://raw.githubusercontent.com/SystemDisc/auto-bet/master/auto-bet.user.js
 // @include       https://www.bitdice.me/
 // @include       https://www.litedice.me/
 // @include       https://www.dogedice.me/
 // @include       https://www.reddice.me/
 // @run-at        document-end
 // @grant         GM_xmlhttpRequest
-// @version       1.10
+// @version       1.11
 // ==/UserScript==
 
 GM_xmlhttpRequest({
