f108a604ede8775e53e882e605432f5c0df238e4
Timothy Zorn <tim@zornco.com>
2014-10-07 19:29:59 -0400

    fixed userscript

--- auto-tip.user.js
+++ auto-tip.user.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name          Auto-Bet userscript
+// @name          Auto-Tip userscript
 // @namespace     https://zornco.com
-// @description	  This will use the most recent version of the auto-bet script at all times
+// @description	  This will use the most recent version of the auto-tip script at all times
 // @author        SystemDisc
-// @downloadURL   https://raw.githubusercontent.com/SystemDisc/auto-bet/master/auto-bet.user.js
+// @downloadURL   https://raw.githubusercontent.com/SystemDisc/auto-bet/master/auto-tip.user.js
 // @include       https://www.bitdice.me/
 // @include       https://www.litedice.me/
 // @include       https://www.dogedice.me/
 // @include       https://www.reddice.me/
 // @run-at        document-end
 // @grant         GM_xmlhttpRequest
-// @version       1.12
+// @version       1.01
 // ==/UserScript==
 
 GM_xmlhttpRequest({
